dce9e563122d95aad73d65b578c5c8302a5e3e35
Álvaro Ramírez <alvaroramirez1982@gmail.com>
2014-01-14 17:43:04 +0100

    Ahora funciona la pantalla de sign-in de dropbox. Pero el funcionamiento no es el esperado puesto que aparece aun el login anterior. Pendiente cambios, y consultar libros...

--- prueba/src/com/example/prueba/Login.java
+++ prueba/src/com/example/prueba/Login.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -11,20 +13,33 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.android.AuthActivity;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
+import com.dropbox.client2.session.Session.AccessType;
+
 public class Login extends Activity {
 
-	/*
-	 * Esta Activity es el Login.
-	 */
+	// Claves para usar y autenticar nuestra App con Dropbox
+	final static private String APP_KEY = "aqlpzm4pt4juvm4";
+	final static private String APP_SECRET = "k2lb0m80cywmkso";
+	final static private AccessType ACCESS_TYPE = AccessType.DROPBOX;
+
+	private DropboxAPI<AndroidAuthSession> mDBApi;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+
+		inicializar();
+
 		// Localizar los controles
 		Button buttondc = (Button) findViewById(R.id.buttondc); // cancelar
 		Button buttondl = (Button) findViewById(R.id.buttondl); // login
-
 		buttondc.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -39,43 +54,35 @@ public class Login extends Activity {
 				// vamos directo a la lista de libros.
 				EditText edtLogin = (EditText) findViewById(R.id.editText1);
 				String login = edtLogin.getText().toString();
-
 				EditText edtPass = (EditText) findViewById(R.id.editText2);
 				String pass = edtPass.getText().toString();
 
-				// CheckBox chbRec = (CheckBox) findViewById(R.id.)
-
-				// tenemos el login y el pass, se manda a la api de dropbox...
-				// fin dropbox
-				// Ahora abrimos la Activity lista
-				// falta comprobar si está activado el chekbox y usando
-				// sharedpreferences...
-				SharedPreferences recuerdame = getSharedPreferences("rec",
-						Context.MODE_PRIVATE);
-				SharedPreferences.Editor editor = recuerdame.edit();
-				// if (){ el checkbox esta activado
-				editor.putBoolean(login, true);
-				editor.commit();
-
-				// Creamos el Intent
+				/*
+				 * Para recordar el inicio de sesión, usamos sharedpreferences
+				 * 
+				 * Lo dejamos comentado para futuras mejoras...
+				 * 
+				 * SharedPreferences recuerdame = getSharedPreferences("rec",
+				 * Context.MODE_PRIVATE); SharedPreferences.Editor editor =
+				 * recuerdame.edit(); // if (){ el checkbox esta activado
+				 * editor.putBoolean(login, true); editor.commit();
+				 */
+				mDBApi.getSession().startAuthentication(Login.this);
+				/* Creamos el Intent
 				Intent intent = new Intent(Login.this, Libros.class);
 				// Iniciamos la nueva actividad
 				startActivity(intent);
-				finish(); // para matar esta pantalla, y no se pueda volver a
-							// ella
+				finish();
+				*/
 			}
 		});
 
 	}
 
-	/*
-	 * El siguiente apartado lo podíamos borrar, puesto que no habrá menús.
-	 */
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	private void inicializar() {
+		AppKeyPair appKeys = new AppKeyPair(APP_KEY, APP_SECRET);
+		AndroidAuthSession session = new AndroidAuthSession(appKeys,
+				ACCESS_TYPE);
+		mDBApi = new DropboxAPI<AndroidAuthSession>(session);
 	}
-
 }
